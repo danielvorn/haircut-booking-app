bd41efa390ac34c90d1164de172bf159a7a070ac
danielvorn <danielvorn@gmail.com>
2023-06-07 21:07:16 +0700

    refactoring package json

--- backend/tests/middleware.tests.ts
+++
@@ -1,8 +0,0 @@
-import request from "supertest";
-import { app } from "../src/index";
-
-describe("Authentication and Authorization", () => {
-  it("should redirect unauthenticated users to the login page", async () => {
-    const res = await request(app).get("/protected");
-  });
-});
